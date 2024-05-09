1d55f6eed6d3c698c753ee7cde0b18107c4b0c30
Harry Ng <harryworld@gmail.com>
2024-05-09 10:13:31 +0800

    HStack

--- Foodie/ContentView.swift
+++ Foodie/ContentView.swift
@@ -2,25 +2,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Button("Tap me!") {
-                print(type(of: self.body))
-            }
-            .frame(width: 100, height: 100)
-            .background(.red)
-            
-            Text("Hello, world!")
-                .padding()
-                .background(.red)
-                .padding()
-                .background(.blue)
-                .padding()
-                .background(.green)
-                .padding()
-                .background(.yellow)
+        HStack {
+            RoundedRectangle(cornerRadius: 8)
+                .foregroundStyle(.red)
+                .frame(width: 100, height: 100)
+            RoundedRectangle(cornerRadius: 8)
+                .foregroundStyle(.green)
+                .frame(width: 100, height: 100)
+            RoundedRectangle(cornerRadius: 8)
+                .foregroundStyle(.blue)
+                .frame(width: 100, height: 100)
         }
-        .font(.largeTitle)
-        .fontWeight(.black)
     }
 }
 
