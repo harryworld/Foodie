d71c1b29d8b17f5dffc6deee3938bc15f7e8b60f
Harry Ng <harryworld@gmail.com>
2024-05-09 19:27:39 +0800

    Basic CardView

--- Foodie/CardView.swift
+++ Foodie/CardView.swift
@@ -2,11 +2,30 @@ import SwiftUI
 
 struct CardView: View {
     var body: some View {
-        VStack {
-            RoundedRectangle(cornerRadius: 16)
-                .fill(.blue)
-                .frame(width: 300, height: 300)
+        ZStack(alignment: .leading) {
+            Image(.papayaSalad)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 600)
+                .cornerRadius(10)
+            
+            VStack(alignment: .leading) {
+                Spacer()
+                
+                Text("Papaya Salad")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                
+                Text("Pun Pun Market")
+                    .font(.body)
+            }
+            .frame(height: 600)
+            .padding()
+            .padding(.bottom, 20)
+            .foregroundStyle(.white)
         }
+        .shadow(radius: 8)
+        .padding()
     }
 }
 
