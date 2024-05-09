96e0a496046b5c0af83b606e230421ed529dbe44
Harry Ng <harryworld@gmail.com>
2024-05-09 19:17:37 +0800

    ZStack

--- Foodie/ContentView.swift
+++ Foodie/ContentView.swift
@@ -10,15 +10,23 @@ struct ContentView: View {
                 .shadow(radius: 8)
                 .offset(y: 10)
             
-            VStack {
-                Image(systemName: "hourglass")
-                    .font(.largeTitle)
+            ZStack {
+                VStack {
+                    Image(systemName: "hourglass")
+                        .font(.largeTitle)
+                    
+                    Text("Tasting more food...")
+                        .font(.title)
+                }
+                .fontWeight(.bold)
+                .frame(maxHeight: 600)
                 
-                Text("Tasting more food...")
-                    .font(.title)
+                VStack {
+                    RoundedRectangle(cornerRadius: 16)
+                        .fill(.blue)
+                        .frame(width: 300, height: 300)
+                }
             }
-            .fontWeight(.bold)
-            .frame(maxHeight: 600)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(.regularMaterial)
