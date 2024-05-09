4d50108aa7e18c29d1775708cef200f18f71f9af
Harry Ng <harryworld@gmail.com>
2024-05-09 10:15:23 +0800

    Starter project

--- Foodie/ContentView.swift
+++ Foodie/ContentView.swift
@@ -2,17 +2,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            RoundedRectangle(cornerRadius: 8)
+        VStack {
+            Image(systemName: "sparkles")
+                .imageScale(.large)
                 .foregroundStyle(.red)
-                .frame(width: 100, height: 100)
-            RoundedRectangle(cornerRadius: 8)
-                .foregroundStyle(.green)
-                .frame(width: 100, height: 100)
-            RoundedRectangle(cornerRadius: 8)
-                .foregroundStyle(.blue)
-                .frame(width: 100, height: 100)
+                .shadow(radius: 10)
+            
+            Text("Hello, SwiftUI!")
+                .font(.largeTitle)
+                .fontWeight(.black)
         }
+        .padding()
     }
 }
 
