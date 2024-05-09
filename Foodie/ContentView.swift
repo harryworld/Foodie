cce05fae414aabfc297149e20e0dc043dffb03db
Harry Ng <harryworld@gmail.com>
2024-05-09 09:29:52 +0800

    Initial Commit

---
+++ Foodie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Foodie
+//
+//  Created by Harry Ng on 9/5/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Foodie/FoodieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieApp.swift
+//  Foodie
+//
+//  Created by Harry Ng on 9/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
