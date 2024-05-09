8edb193e24cc1d38828b98887722f8d270cbab36
Harry Ng <harryworld@gmail.com>
2024-05-09 09:52:33 +0800

    Embed in VStack

--- Foodie/ContentView.swift
+++ Foodie/ContentView.swift
@@ -11,6 +11,13 @@ struct ContentView: View {
             Text("Hello, SwiftUI!")
                 .font(.largeTitle)
                 .fontWeight(.black)
+            
+            VStack {
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(.blue)
+                    .frame(width: 300, height: 300)
+                Text("1st lesson")
+            }
         }
         .padding()
     }
